5bb52c9ad84c8399de656aa4bd28c967defa0870
EkyMamba <74607076+EkyMamba@users.noreply.github.com>
2024-07-18 08:01:53 +0700

    Create 4.9_icd9_afib.sql

---
+++ 1_extract_bigquery/4.9_icd9_afib.sql
@@ -0,0 +1,15 @@
+-- Atrial Fibrillation ICD-9
+CREATE OR REPLACE TABLE `mimic-iv-ekymamba-2002.mimiciv_afib_finish.mimiciv_afib_icd9` AS
+SELECT DISTINCT
+  stay_id,
+  subject_id,
+  hadm_id,
+  activity,
+  timestamps,
+  category,
+  statusdescription,
+  icd_code,
+  disease_name
+FROM `mimic-iv-ekymamba-2002.mimiciv_afib_finish.mimiciv_afib_eventlog`
+WHERE icd_code = '42731'
+ORDER BY stay_id, timestamps
