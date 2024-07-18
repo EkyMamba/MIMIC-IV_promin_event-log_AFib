8f172e9b87879725e3c343799877ce48dd76d550
EkyMamba <74607076+EkyMamba@users.noreply.github.com>
2024-07-18 08:00:28 +0700

    Create 4.7_permanent_afib.sql

---
+++ 1_extract_bigquery/4.7_permanent_afib.sql
@@ -0,0 +1,15 @@
+-- Permanent Atrial Fibrillation
+CREATE OR REPLACE TABLE `mimic-iv-ekymamba-2002.mimiciv_afib_finish.mimiciv_afib_permanent` AS
+SELECT DISTINCT
+  stay_id,
+  subject_id,
+  hadm_id,
+  activity,
+  timestamps,
+  category,
+  statusdescription,
+  icd_code,
+  disease_name
+FROM `mimic-iv-ekymamba-2002.mimiciv_afib_finish.mimiciv_afib_eventlog`
+WHERE icd_code = 'I4821'
+ORDER BY stay_id, timestamps
