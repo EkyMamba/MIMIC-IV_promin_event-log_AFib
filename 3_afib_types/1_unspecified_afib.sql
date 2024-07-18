a16472c458c64a2fa2866797c4966d8065bd40b5
EkyMamba <74607076+EkyMamba@users.noreply.github.com>
2024-07-18 12:38:05 +0700

    Update and rename 4.1_unspecified_afib.sql to 1_unspecified_afib.sql

--- 3_afib_types/1_unspecified_afib.sql
+++ 3_afib_types/1_unspecified_afib.sql
@@ -1,4 +1,3 @@
--- Unspecified Atrial Fibrillation
 CREATE OR REPLACE TABLE `mimic-iv-ekymamba-2002.mimiciv_afib_finish.mimiciv_afib_unspecified` AS
 SELECT DISTINCT
   stay_id,
@@ -10,6 +9,6 @@ SELECT DISTINCT
   statusdescription,
   icd_code,
   disease_name
-FROM `mimic-iv-ekymamba-2002.mimiciv_afib_finish.mimiciv_afib_eventlog`
+FROM `mimic-iv-ekymamba-2002.mimic_insights.icu_af_eventlog`
 WHERE icd_code = 'I4891'
 ORDER BY stay_id, timestamps
