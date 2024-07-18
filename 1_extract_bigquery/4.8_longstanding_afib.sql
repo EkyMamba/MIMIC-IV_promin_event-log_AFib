c2b89c2e4cc2e983304babe46836303f26f1287f
EkyMamba <74607076+EkyMamba@users.noreply.github.com>
2024-07-18 08:01:20 +0700

    Create 4.8_longstanding_afib.sql

---
+++ 1_extract_bigquery/4.8_longstanding_afib.sql
@@ -0,0 +1,15 @@
+-- Longstanding Atrial Fibrillation
+CREATE OR REPLACE TABLE `mimic-iv-ekymamba-2002.mimiciv_afib_finish.mimiciv_afib_longstanding` AS
+SELECT DISTINCT
+  stay_id,
+  subject_id,
+  hadm_id,
+  activity,
+  timestamps,
+  category,
+  statusdescription,
+  icd_code,
+  disease_name
+FROM `mimic-iv-ekymamba-2002.mimiciv_afib_finish.mimiciv_afib_eventlog`
+WHERE icd_code = 'I4811'
+ORDER BY stay_id, timestamps
