932fd2f90939c18f1f0f97d23604dfa6f285a2b1
EkyMamba <74607076+EkyMamba@users.noreply.github.com>
2024-07-18 07:54:26 +0700

    Create 4.2_paroxysmal_afib.sql

---
+++ 1_extract_bigquery/4.2_paroxysmal_afib.sql
@@ -0,0 +1,15 @@
+-- Paroxysmal Atrial Fibrillation
+CREATE OR REPLACE TABLE `mimic-iv-ekymamba-2002.mimiciv_afib_finish.mimiciv_afib_paroxysmal` AS
+SELECT DISTINCT
+  stay_id,
+  subject_id,
+  hadm_id,
+  activity,
+  timestamps,
+  category,
+  statusdescription,
+  icd_code,
+  disease_name
+FROM `mimic-iv-ekymamba-2002.mimiciv_afib_finish.mimiciv_afib_eventlog`
+WHERE icd_code = 'I480'
+ORDER BY stay_id, timestamps
